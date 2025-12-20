908672393e71d4a719c0f490f53553f414f240d2
abx15 <arunkumarbind150720@gmail.com>
2025-12-20 12:25:12 +0530

    auth handel

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout, sendOtp, verifyOtp, resetPassword } from '../controllers/auth.controllers.js';
+import { signup, login, logout, sendOtp, verifyOtp, resetPassword, googleAuth } from '../controllers/auth.controllers.js';
 
 const authRouter = express.Router();
 
@@ -9,5 +9,7 @@ authRouter.post("/send-otp", sendOtp);
 authRouter.post("/verify-otp", verifyOtp);
 authRouter.post("/reset-password", resetPassword);
 authRouter.get("/logout", logout);
+authRouter.post("google-auth",googleAuth);
+
 
 export default authRouter;
