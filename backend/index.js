09092021228b93fdc9b691acfb5e73f347c8dd5c
abx15 <arunkumarbind150720@gmail.com>
2026-01-13 10:16:01 +0530

    port chenges

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+
 import authRouter from './routes/auth.routes.js';
 import orderRouter from './routes/order.routes.js';
 import userRouter from './routes/user.routes.js';
@@ -16,27 +16,32 @@ connectDB();
 
 const app = express();
 const server = createServer(app);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://zest-eats.vercel.app"
+];
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
 
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 
-  }
-));
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
 
-// Make io accessible to our routers
+// Make io accessible to routers
 app.set('io', io);
 
 app.use("/api/auth", authRouter);
